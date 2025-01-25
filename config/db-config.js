252a47e48e2fe8c97d0dd26af1d3308b8cd6799e
yonas-t-a <yonas.tessema@aait.edu.et>
2025-01-25 13:03:12 +0300

    change the configration of the databsed for the user model to the db-config

--- config/db-config.js
+++ config/db-config.js
@@ -1,4 +1,3 @@
-import mysql from 'mysql2';
 import dotenv from 'dotenv';
 import { Sequelize, DataTypes } from 'sequelize';
 dotenv.config();
@@ -11,4 +10,35 @@ export const sequelizeConnection = new Sequelize({
     host: process.env.MySQL_HOST,
     port: process.env.MySQL_PORT || 3306,
 });
+
+
+/**
+ * The user object has the following Properties
+ * @property {INTEGER} id : userId Authinc
+ * @property {STRING} name: UserName not NUll,
+ * @property {STRING} email: UserEmail not null.
+ */
+export const user = sequelizeConnection.define('User',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    }
+},
+{
+    tableName: 'Users',
+    freezeTableName: true,
+    timestamps: false
+}
+)
  
