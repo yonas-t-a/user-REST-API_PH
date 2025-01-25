dcaf0c50f396a3a73ee265fecce5907965f37d0b
yonas-t-a <yonas.tessema@aait.edu.et>
2025-01-25 12:42:55 +0300

    configure database

--- config/db-config.js
+++ config/db-config.js
@@ -1,3 +1,14 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
+import { Sequelize, DataTypes } from 'sequelize';
 dotenv.config();
+
+export const sequelizeConnection = new Sequelize({
+    dialect: 'mysql',
+    database: process.env.MySQL_DATABASE,
+    username: process.env.MySQL_USER,
+    password: process.env.MySQL_PASSWORD,
+    host: process.env.MySQL_HOST,
+    port: process.env.MySQL_PORT || 3306,
+});
+ 
