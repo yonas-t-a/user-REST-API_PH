cbdb3e5a4e0dedab3459a191138385830dd721b6
yonas-t-a <yonas.tessema@aait.edu.et>
2025-01-28 12:26:34 +0300

    create a configration to database

--- config/db-config.js
+++ config/db-config.js
@@ -11,34 +11,28 @@ export const sequelizeConnection = new Sequelize({
     port: process.env.MySQL_PORT || 3306,
 });
 
-
-/**
- * The user object has the following Properties
- * @property {INTEGER} id : userId Authinc
- * @property {STRING} name: UserName not NUll,
- * @property {STRING} email: UserEmail not null.
- */
-export const user = sequelizeConnection.define('User',{
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+export const user = sequelizeConnection.define(
+    'user',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
     },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
+    {
+        tableName: 'Users', 
+        freezeTableName: true, 
+        timestamps: false, 
     }
-},
-{
-    tableName: 'Users',
-    freezeTableName: true,
-    timestamps: false
-}
-)
- 
+);
