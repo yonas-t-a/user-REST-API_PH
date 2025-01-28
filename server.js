1e2036c9ee3cf867e601ee93369b3aa7238318b7
yonas-t-a <yonas.tessema@aait.edu.et>
2025-01-28 12:29:05 +0300

    set up the route for the api

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
 
+import router from './API/user_API.js';
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/users', router)
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
